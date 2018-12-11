426a028a39907f9329601791e41c87d0e4190839
NoahAsaria <40721601+NoahAsaria@users.noreply.github.com>
2018-12-10 22:30:29 -0600

    Add files via upload

--- src/main/java/com/example/dao/BorrowerDAO.java
+++ src/main/java/com/example/dao/BorrowerDAO.java
@@ -8,16 +8,20 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.domain.Borrower;
 import com.example.demo.domain.BorrowerMapper;
-
+import com.example.demo.domain.*;
 
 @Component
 public class BorrowerDAO {
 	
 	@Autowired
 	BorrowerMapper borrowerMapper;
+	Book_LoansMapper bookloanMapper;
 	
 	public BorrowerDAO() {
 		
@@ -26,7 +30,6 @@ public class BorrowerDAO {
 	public List<Borrower> getAllBorrowerInfo() {
 		List<Borrower> borrowerList = new ArrayList<Borrower>();	
 		borrowerList = borrowerMapper.getAllBorrowerInfo();
-		
 		return borrowerList;
 	}
 
@@ -35,5 +38,25 @@ public class BorrowerDAO {
 		
 	}
 	
+	public List<CheckedOutBooks> getCheckedOut(Borrower borrower)
+	{
+		List<CheckedOutBooks> checkedOutList = new ArrayList<CheckedOutBooks>();
+		checkedOutList = bookloanMapper.getCheckedOut(borrower);
+		return checkedOutList;
+	}
+	public boolean checkLogin(String cardno, String pass)
+	{
+		List<Borrower> borrowerList = new ArrayList<Borrower>();
+		System.out.println((cardno == null) + "cardno = " + cardno);
+		System.out.println((pass == null + "pass = " + pass));
+		System.out.println("In the DAO");
+		borrowerList = borrowerMapper.checkLogin(cardno, pass);
+		System.out.println("Passed the mapper");
+		if (borrowerList.isEmpty()) //If no match populated.
+		{
+			return false;
+		}
+		else return true;
+	}
 
 }
